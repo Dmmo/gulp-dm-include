4f537d8e74f182cb1ba4c58c91066ee91f41dcf4
Dm <ilostinyoureyes@gmail.com>
2016-04-14 11:21:23 +0800

    同步npmjs代码库

--- README.md
+++ README.md
@@ -1,2 +1,102 @@
-# gulp-dm-include
-gulp解析模板插件
+#### 查看gulp-dm-include 配置文件
+安装完毕后查看src/public/conf/rule.js
+类似于下面这样：
+```javascript
+var config = {
+    main: [
+        {
+            type: 'javascript',
+            value: [
+                'public/lib/zepto/zepto.min.js',
+                'public/lib/zepto/touch.min.js',
+                'public/lib/underscore.min.js',
+                'public/lib/iscroll',
+                'public/js/common',
+                'public/lib/oop',
+                'public/js/D',
+                'public/native/platformAdapter.js',
+                'public/native/api.js'
+            ]
+        }
+    ],
+
+    head: [
+        {
+            type: 'template',
+            value: [
+                'src/public/include/header.html'
+            ]
+        },
+        {
+            type: 'stylesheel',
+            value: [
+                'public/style/comm'
+            ]
+        }
+    ]
+};
+
+module.exports = config;
+```
+gulp-dm-include 支持三种（js、html、css）文件的引用，所对应的分别为: javascript、template、stylesheel。
+type参数必填只要指定引用类型可忽略后缀名。
+
+gulp-dm-include 支持多文件的引用. 例如：
+```javascript
+    main: [
+        {
+            type: 'javascript',
+            value: [
+                'public/lib/zepto/zepto.min.js',
+                'public/lib/zepto/touch.min.js',
+                'public/lib/underscore.min.js',
+                'public/lib/iscroll',
+                'public/js/common',
+                'public/lib/oop',
+                'public/js/D',
+                'public/native/platformAdapter.js',
+                'public/native/api.js'
+            ]
+        }
+    ],
+```
+#### 页面中使用gulp-dm-include
+```html
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>首页</title>
+    <!-- dm-include rule="head"-->
+    <!-- dm-include stylesheel="modules/dmTest/css/style;modules/dmTest/css/comm.css" -->
+</head>
+<body>
+
+<div id="test" style="width: 60px; height: 30px; background:red;"></div>
+
+<div id="testTplContainer">
+
+</div>
+<script type="text/template" id="testTpl">
+    <%
+        _.each(arr, function(i){
+    %>
+        <h1><%= i%></h1>
+    <%
+        });
+    %>
+</script>
+<!-- dm-include rule="main" -->
+<!-- dm-include javascript="modules/dmTest/js/index" -->
+</body>
+</html>
+```
+#### 页面中gulp-dm-include指令说明
+##### 引用一组rule
+`<!-- dm-include rule="head" --> `
+指明引用类型为rule，将通过public/conf/rule.js里面的规则引用.
+##### 引用多个样式表
+`<!-- dm-include stylesheel="modules/dmTest/css/style;modules/dmTest/css/comm.css" -->`
+指明引用类型为stylesheel，多个样式引用以英文分号（;）隔开。可忽略后缀名。
+##### 引用多个js文件
+<!-- dm-include javascript="modules/dmTest/js/index;modules/dmTest/js/test" -->
+指明引用类型为javascript，多个样式引用以英文分号（;）隔开。可忽略后缀名。
---
+++ index.js
@@ -0,0 +1,216 @@
+var gutil = require('gulp-util'),
+    through = require('through2'),
+    path = require('path'),
+    fs = require('fs'),
+    PluginError = gutil.PluginError,
+    PLUGIN_NAME = 'gulp-dm-include',
+    reg = /<!--\s*dm\-include\s+([^=]+)\s*=\s*['"]([^'"]+)['"]\s*-->/g,
+    reg2 = /<!--\s*dm\-include\s+(\w+)\s*=\s*['"]([^'"]+)['"]\s*-->/,
+    _options,
+    _self;
+
+
+/**
+ * 解析javascript
+ * @param value [type=array]
+ * @returns {*}
+ */
+var analysisJavascript = function(value, file){
+
+    var returns = '',
+        defer = _options.isDefer ? 'defer="defer"' : '',
+        typeReg = /\.[^\.]+$/,
+        type,
+        tmpValue,
+        tmp;
+
+    if(value && value.length > 0 ){
+        for (var i = 0; i < value.length; i++){
+            tmpValue = value[i];
+            type = typeReg.exec(tmpValue);
+
+            if(type === null){
+                tmpValue = tmpValue + '.js';
+            }
+
+            if(type !== null && type[0] !== '.js'){
+                throw new PluginError(PLUGIN_NAME, '引入javascript['+tmpValue+']文件失败, 请确保您的js文件后缀为.js');
+            }
+            tmpValue = _options.ENV+path.sep+tmpValue;
+            tmp = path.relative(path.dirname(_options.ENV+path.sep+path.relative('src',file.path)), tmpValue);
+            returns += (i < value.length - 1) ? '<script ' + defer + ' type="text/javascript" src="' + tmp + '"></script>\r\n' :'<script ' + defer + ' type="text/javascript" src="' + tmp + '"></script>';
+        }
+    }
+    return returns;
+};
+
+/**
+ * 解析器: Stylesheel
+ * @param value [type=array]
+ * @returns {*}
+ */
+var analysisStylesheel = function(value, file) {
+    var returns = '',
+        typeReg = /\.[^\.]+$/,
+        type,
+        tmpValue,
+        tmp;
+
+    for(var i = 0; i < value.length; i++ ){
+        tmpValue = value[i];
+        type = typeReg.exec(tmpValue);
+        //如果没有写后缀名就默认添加.css后缀
+        if(type === null){
+            tmpValue = tmpValue + '.css';
+        }
+
+        if(type !== null && type[0] !== '.css'){
+            throw new PluginError(PLUGIN_NAME, '引入css['+tmpValue+']文件失败, 请确保您的css文件后缀为.css');
+        }
+        tmpValue = _options.ENV+path.sep+tmpValue;
+        tmp = path.relative(path.dirname(_options.ENV+path.sep+path.relative('src',file.path)), tmpValue);
+        returns += ( i === 0)  ? '<link rel="stylesheet" href="' + tmp + '"/>\r\n' : '\t<link rel="stylesheet" href="' + tmp + '"/>\r\n';
+    }
+
+    return returns;
+};
+
+
+var analysisTemplate = function(value, file) {
+    var returns= '',
+        typeReg = /\.[^\.]+$/,
+        type,
+        tmpValue;
+
+    for (var i = 0; i< value.length; i++){
+        tmpValue = value[i];
+        type = typeReg.exec(tmpValue);
+
+        if(type === null){
+            tmpValue = tmpValue + '.html';
+        }
+
+        if(type !== null && type[0] !== '.html'){
+            throw new PluginError(PLUGIN_NAME, '引入模板['+tmpValue+']文件失败, 请确保您的模板文件后缀为.html');
+        }
+
+        returns += fs.readFileSync(tmpValue, {encoding: 'utf-8'}) + "\r\n";
+
+    }
+
+    return returns;
+};
+
+
+/**
+ * 根据指令及传入的值返回
+ * @param control
+ * @param value
+ */
+var analysisValue = function(control, value, file){
+    value = value.indexOf(';') === -1 ? value : value.split(';');
+    var returns = '',
+        typeReg = /\.[^\.]+$/,
+        type,
+        tmp;
+
+    switch (control){
+
+        // javascript处理
+        case 'javascript':
+
+            // 一条标签引用多个javascript文件处理
+            if(toString.apply(value) === '[object Array]'){
+                returns = analysisJavascript(value, file);
+            }else{ //一条标签引用一个javascript文件处理
+                returns = analysisJavascript([value], file);
+            }
+            break;
+
+        // 处理Stylesheel样式表
+        case 'stylesheel':
+            if(toString.apply(value) === '[object Array]'){
+                returns = analysisStylesheel(value, file);
+            }else{
+                returns = analysisStylesheel([value], file);
+            }
+            break;
+
+        // 处理一组规则
+        case 'rule':
+            var tmpValue;
+            for(var i in _options.rules){
+                if(i === value){
+                    tmpValue = _options.rules[i];
+                }
+            }
+
+            for(var n = 0; n < tmpValue.length; n++ ){
+                var item = tmpValue[n];
+                switch(item.type){
+                    case 'javascript':
+                        returns += analysisJavascript(item.value, file);
+                        break;
+                    case 'stylesheel':
+                        returns += analysisStylesheel(item.value, file);
+                        break;
+                    case 'template':
+                        returns += analysisTemplate(item.value, file);
+                        break;
+                }
+            }
+            break;
+
+        case 'template':
+            if(toString.apply(value) === '[object Array]'){
+                returns = analysisTemplate(value, file);
+            }else{
+                returns = analysisTemplate([value], file);
+            }
+            break;
+        default:
+            throw new PluginError(PLUGIN_NAME, '不支持该类型:' + control);
+            break;
+    }
+    return returns;
+};
+
+
+var DMInclude = function(options){
+    return through.obj(function( file, enc, cb){
+        options = options || {};
+        options.rules = require(options.rule);
+        _options = options;
+        _self = this;
+
+        if(file.isNull()){
+            this.push(file);
+            return cb();
+        }
+
+        if(file.isStream()){
+            this.emit('error', new gutil.PluginError(PLUGIN_NAME, 'Streaming out supported'));
+            return cb();
+        }
+
+        var html = file.contents.toString(),
+            matches = html.match(reg),
+            control,
+            value,
+            filePath;
+
+        if(matches){
+            for(var i = 0; i < matches.length; i++){
+                var tmp = matches[i].match(reg2);
+                control = tmp[1];
+                value   = tmp[2];
+                html = html.replace(matches[i], analysisValue(control, value, file));
+            }
+        }
+        file.contents = new Buffer(html);
+        this.push(file);
+        cb();
+    });
+};
+
+module.exports = DMInclude;
